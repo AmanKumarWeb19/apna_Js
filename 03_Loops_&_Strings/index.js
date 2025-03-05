086c11988565b50f0d148ee283a09df5e79227fb
Aman Kumar <akgupta0524@gmail.com>
2025-03-05 09:25:14 +0530

    print number from 1 to 100

--- 03_Loops_&_Strings/index.js
+++ 03_Loops_&_Strings/index.js
@@ -1,5 +1,21 @@
-// Q-1 Print all the Even  Number(0 - 100)
+// Q-1 Print all the Number(0 - 100)
 
 for (let i = 1; i <= 100; i++) {
-  console.log("number", i);
+  console.log("number =", i);
+}
+
+// Q-1 Print all the Even Number(0 - 100)
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log("Even Number =", i);
+  }
+}
+
+// Q-1 Print all the Odd Number(0 - 100)
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log("Odd Number =", i);
+  }
 }
