08a67063e7c6893709851d028f2a61b0e859c871
Aman Kumar <akgupta0524@gmail.com>
2025-03-06 08:04:20 +0530

    print all element

---
+++ 04_Arrays/index.js
@@ -0,0 +1,7 @@
+/** Print All element from an Array. */
+
+let heroes = ["hulk", "ironman", "superman", "thor", "spiderman", "batman"];
+
+for (let i = 0; i < heroes.length; i++) {
+  console.log(heroes[i]);
+}
