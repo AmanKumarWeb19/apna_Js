ec0f569e6a847bb4145e5411a833796c6042dd1a
Aman Kumar <akgupta0524@gmail.com>
2025-03-06 09:10:02 +0530

    check push and pop

--- 04_Arrays/index.js
+++ 04_Arrays/index.js
@@ -2,6 +2,13 @@
 
 let heroes = ["hulk", "ironman", "superman", "thor", "spiderman", "batman"];
 
-for (let i = 0; i < heroes.length; i++) {
-  console.log(heroes[i]);
+// for (let i = 0; i < heroes.length; i++) {
+//   console.log(heroes[i]);
+// }
+
+let heroesName = ["hulk", "ironman", "superman", "thor", "spiderman", "batman"];
+for (let i = 0; i < heroesName.length; i++) {
+  console.log(heroesName[i]);
 }
+heroesName.push("jellyman");
+console.log(heroesName);
